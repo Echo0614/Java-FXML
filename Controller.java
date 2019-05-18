115ab6461a1adbbd37b41cefe670330c5fae40fa
Echo0614 <50775639+Echo0614@users.noreply.github.com>
2019-05-18 22:52:20 +0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,13 +1,19 @@
 package sample;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable
+public class Controller
 {
-    public void initialize(URL location, ResourceBundle resources)
-    {
-        System.out.println("View is now loaded!");
-    }
+	public Button someButton; //refers to the id in sample.fxml
+	
+	//method that tied to item in FXML (button, label, etc)
+	public void handleButtonClicked()
+	{
+		System.out.println("Print out in terminal");
+		someButton.setText("Stop clicking me!");
+	}
 }
